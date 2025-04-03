5fd4e9370c1929205586d12db09f75866c4641e6
eucristiancabral <cristianjosecabral@outlook.com>
2025-04-03 20:58:06 -0300

    feat(c): starts basic program structure
    This structure will serve as a basis for the implementation of future operations.
    
    Add the main function and declare variables to store the input numbers, operator, and result.

---
+++ calc.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //Standard input and output library
+
+/*Main function*/
+int main(){
+    float n1, n2, res; //Float variable for inputting values ​​and the result 
+    int ope; //Int variable to store the operator option chosen by the user
+    
+
+    return 0;
+}
