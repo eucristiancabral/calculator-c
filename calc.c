5563adaf5d1d41faeb41ff7a11434d8c2e10da76
eucristiancabral <cristianjosecabral@outlook.com>
2025-04-03 21:02:47 -0300

    feat(c): adds user input of numbers
    Implements displaying the calculator title and prompting the user to enter two numbers.
    
    The entered values ​​are stored in float variables to be used in future calculations, ensuring precision in arithmetic operations. This allows handling decimal numbers accurately, which is essential for performing mathematical computations with non-integer values.

--- calc.c
+++ calc.c
@@ -5,6 +5,17 @@ int main(){
     float n1, n2, res; //Float variable for inputting values ​​and the result 
     int ope; //Int variable to store the operator option chosen by the user
     
+    // Display the calculator title
+    printf("------ CALCULADORA EM C ------\n"); 
+
+    // Ask the user for the first number
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &n1); // Reads a floating point number (float) entered by the user and stores it in n1
+
+    // Ask the user for the second number
+    printf("Digite o segundo numero: ");
+    scanf("%f", &n2); // Reads a floating point number (float) entered by the user and stores it in n2
+
 
     return 0;
 }
