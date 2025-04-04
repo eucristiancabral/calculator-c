04c9e9197732de4223b370cd88d62fb88550a442
eucristiancabral <cristianjosecabral@outlook.com>
2025-04-03 21:35:06 -0300

    feat(c): implements calculation logic with conditional structure
    Adds a switch case to perform the mathematical operation corresponding to the user's choice.
    
    Implements addition, subtraction, multiplication, and division functions, ensuring that division by zero is handled with an error message. It also includes a default case to handle invalid input, terminating and making the program more robust and interactive.

--- calc.c
+++ calc.c
@@ -25,5 +25,33 @@ int main(){
     printf("4 - Divisao\n");
     scanf("%d", &ope); // Reads an integer entered by the user and stores it in the 'ope' variableg
 
-    return 0;
+    //Conditional structure 
+    switch (ope) {
+        case 1: // If the 'ope' variable is equal to 1, this block will be executed.
+            res = n1 + n2;
+            printf("Resultado: %.2f", res);
+            break; // Ends the execution of the current block
+        case 2:
+            res = n1 - n2;
+            printf("Resultado: %.2f", res);
+            break;
+        case 3: 
+            res = n1 * n2;
+            printf("Resultado: %.2f", res);
+            break;
+        case 4:
+            if (n2 != 0){ // Check if the divisor is not zero
+            res = n1 / n2;
+            printf("Resultado: %.2f", res);
+            } else{ 
+                // If the second number is zero, display an error message
+                printf("Divisao por zero nao permitida.");
+            }
+            break;
+        default: // If the user enters an invalid option
+            printf("Operador invalido.");
+        break;
+    }   
+
+    return 0; // End the program
 }
